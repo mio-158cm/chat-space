aa8e2ac5d7da54e7ec7794239d05a52d3216149f
mio-158cm <hayakawan18@gmail.com>
2019-05-22 19:54:39 +0900

    space

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,7 @@ class UsersController < ApplicationController
   def index
     @users = User.where('name LIKE(?)', "%#{params[:name]}%" ).where.not(id: current_user)
     respond_to do |format|
-
-      format.json
+    format.json
     end
   end
 
