9693d15ba3914d9e3ce058c0e5af97b8a2f29179
mio-158cm <hayakawan18@gmail.com>
2019-05-24 15:53:52 +0900

    自動更新-過程４

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,7 @@
 $(function(){
 
   function buildHTML(message){
+    var messages = $('tbody').append("");
     // var insertImage = '';
     // if (message.image.url) {
     //   insertImage = `<img src="${message.image.url}">`;
@@ -51,4 +52,28 @@ $('#new_message').on('submit', function(e){
     $('.form__submit').prop('disabled', false);
   })
 })
+//自動更新
+$(function(){
+  setInterval(update, 5000);
+});
+
+function update(){ //
+  if($('.messages')[0]){
+  var message_id = $('.messages:last').data('id');
+  //一番最後にある'messages'というクラスの'id'というデータ属性を取得し、'message_id'という変数に代入
+} else { //ない場合は
+  var message_id = 0 //0を代入
+}
+  $.ajax({
+    url: '/groups/:group_id/messages',
+    type: 'GET',
+    data: {message: { id: message_id }},
+    dataType: 'json'
+  })
+  .always(function(data){ //通信したら、成功しようがしまいが受け取ったデータ（@new_message)を引数にとって以下のことを行う
+    $.each(data, function(i, data){ //'data'を'data'に代入してeachで回す
+      buildMESSAGE(data); //buildMESSAGEを呼び出す
+    });
+  });
+}
 });
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,6 +4,10 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
+    respond_to do |format|
+      format.html # html形式でアクセスがあった場合は特に何もなし(@messages = Message.allして終わり）
+      format.json { @new_message = Message.where('id > ?', params[:message][:id]) } # json形式でアクセスがあった場合は、params[:message][:id]よりも大きいidがないかMessageから検索して、@new_messageに代入する
+    end
   end
 
   def create
