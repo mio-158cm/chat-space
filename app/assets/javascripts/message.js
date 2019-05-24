0a2e4370cddaeed524bbccad893910359f1417e0
mio-158cm <hayakawan18@gmail.com>
2019-05-24 12:34:42 +0900

    自動更新-過程3

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -51,4 +51,29 @@ $('#new_message').on('submit', function(e){
     $('.form__submit').prop('disabled', false);
   })
 })
+  // 自動更新
+var interval = setInterval(function() {
+  if (location.href.match(/\/groups\/\d+\/messages/)){
+    var message_id = $('.main-contents__body__list__message').last().data('id');
+    $.ajax({
+      url: location.href,
+      type: "GET",
+      data: {id: message_id},
+      dataType: "json"
+    })
+    .done(function(data) {
+      data.forEach(function(message) {
+        var html = buildHTML(message);
+        $('.massages').append(html);
+        $(".main-contents__body").animate({scrollTop:$('.main-contents__body__list')[0].scrollHeight});
+        $('.new_message .message').val('');
+      })
+    })
+    .fail(function() {
+      alert('自動更新に失敗しました');
+    });
+  } else {
+      clearInterval(interval);
+    }
+} , 5000 );
 });
