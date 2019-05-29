460e2936214163e39d3de5e5bc2d96140a5ffc5b
mio-158cm <hayakawan18@gmail.com>
2019-05-29 18:57:38 +0900

    yml変更

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -6,7 +6,7 @@ working_directory "#{app_path}/current"
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
-stdout_path "#{app_path}/shared/log/unicorn.stdout.log"PdaP
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
 
 listen 3000
