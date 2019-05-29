d23974611050d4f090a33c2f9440aaef2577545d
mio-158cm <hayakawan18@gmail.com>
2019-05-29 15:31:24 +0900

    setting

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -6,7 +6,7 @@ working_directory "#{app_path}/current"
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
-stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"PdaP
 
 
 listen 3000
